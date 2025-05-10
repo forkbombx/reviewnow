3d51e5f3de76c3661974f1babf6d3d4ab8f692b6
forkbombx <--global>
2025-05-10 12:17:54 +0200

    refactor(src): restructure folders and env handling for better config support

--- src/auth.ts
+++ src/auth.ts
@@ -2,7 +2,7 @@ import NextAuth from "next-auth";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db/index";
 import Resend from "next-auth/providers/resend";
-import { env } from "@/env";
+import { env } from "./env";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
