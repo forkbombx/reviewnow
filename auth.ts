060c4cf99b50fa4caca5df9fba36025d1f174547
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:41 +0200

    feat(EmailProvider): Added Resend to send magic links

--- auth.ts
+++ auth.ts
@@ -1,8 +1,13 @@
-import NextAuth from "next-auth"
-import { DrizzleAdapter } from "@auth/drizzle-adapter"
-import { db } from "@/app/db/index"
+import NextAuth from "next-auth";
+import { DrizzleAdapter } from "@auth/drizzle-adapter";
+import { db } from "@/app/db/index";
+import Resend from "next-auth/providers/resend";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
-  providers: [],
-})
+  providers: [
+    Resend({
+      from: process.env.AUTH_FROM_EMAIL,
+    }),
+  ],
+});
