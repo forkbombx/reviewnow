5b0f67ab2838fdabd8983c6050d294c21e252cc6
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:44 +0200

    refactor(form): Refactored to use  instead of

--- src/components/auth/magic-link-form.tsx
+++ src/components/auth/magic-link-form.tsx
@@ -1,48 +1,24 @@
+"use client";
+
+import { signInAction } from "@/actions/auth";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { authSchema } from "@/lib/form-schema";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { useActionState } from "react";
+import { Label } from "@/components/ui/label";
 
 export function MagicLinkForm() {
-  const form = useForm<z.infer<typeof authSchema>>({
-    resolver: zodResolver(authSchema),
-    defaultValues: {
-      email: "",
-    },
-  });
+  const [state, formAction, isPending] = useActionState(signInAction, null);
 
   return (
-    <div className="flex flex-col gap-6">
-      <Form {...form}>
-        <form className="grid gap-2">
-          <FormField
-            control={form.control}
-            name="email"
-            render={({ field }) => (
-              <FormItem className="grid gap-2">
-                <FormLabel>Email</FormLabel>
-                <FormControl>
-                  <Input placeholder="Email" {...field} />
-                </FormControl>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-        </form>
-      </Form>
-    </div>
+    <form className="grid gap-2" action={formAction}>
+      <div className="grid gap-2">
+        <Label>Email</Label>
+        <Input placeholder="Email" />
+        <p className="text-red-500"> {state?.message} </p>
+      </div>
+      <Button disabled={isPending} type="submit" className="w-full">
+        Login
+      </Button>
+    </form>
   );
 }
