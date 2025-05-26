b559e5c8ac13fe04e9e90cbbcf08db9f85043c33
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:44 +0200

    feat(oauth): Get Github oauth working

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -3,10 +3,22 @@
 import { signIn } from "@/auth";
 import { authSchema } from "@/lib/schema";
 
-async function signInAction(_: any, formData: FormData) {
+async function signInWithMagicLinkAction(_: any, formData: FormData) {
   const schema = authSchema.safeParse(formData.get("email"));
   if (!schema.success) return { message: "Please type a valid email" };
   return await signIn("resend", formData);
 }
 
-export { signInAction };
+async function signInWithGithubAction(_: any, formData: FormData) {
+  return await signIn("github");
+}
+
+async function signInWithGoogleAction(_: any, formData: FormData) {
+  return await signIn("google");
+}
+
+export {
+  signInWithGoogleAction,
+  signInWithGithubAction,
+  signInWithMagicLinkAction,
+};
