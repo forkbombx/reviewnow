c8a333e95130ac8a756ec9de54ebd8bd221cb43c
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:39 +0200

    feat(auth): Redirect after sign in

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -6,15 +6,15 @@ import { authSchema } from "@/lib/schema";
 async function signInWithMagicLinkAction(_: any, formData: FormData) {
   const schema = authSchema.safeParse(formData.get("email"));
   if (!schema.success) return { message: "Please type a valid email" };
-  return await signIn("resend", formData);
+  return await signIn("resend", { redirectTo: "/", formData });
 }
 
-async function signInWithGithubAction(_: any, formData: FormData) {
-  return await signIn("github");
+async function signInWithGithubAction() {
+  return await signIn("github", { redirectTo: "/" });
 }
 
-async function signInWithGoogleAction(_: any, formData: FormData) {
-  return await signIn("google");
+async function signInWithGoogleAction() {
+  return await signIn("google", { redirectTo: "/" });
 }
 
 export {
