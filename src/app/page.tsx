3128a4de2ce3ce6358605736b9105e56dff25675
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:42 +0200

    feat(dashboard): Created a create project section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { CreateProject } from "@/components/dashboard/create-project";
 import { DashboardHeader } from "@/components/dashboard/dashboard-header";
 import { ProjectsGrid } from "@/components/dashboard/projects-grid";
 
@@ -6,6 +7,7 @@ export default function Page() {
     <div className="h-svh md:p-6 md:bg-sidebar">
       <div className="min-h-full bg-background md:rounded-2xl @container/main flex flex-col w-full">
         <DashboardHeader />
+        <CreateProject />
         <div className="flex flex-1 flex-col">
           <div className="@container/main py-6 flex flex-1 flex-col gap-2">
             <ProjectsGrid />
--- src/lib/routes.ts
+++ src/lib/routes.ts
@@ -2,4 +2,8 @@ export const routes = {
   auth: "/auth",
   terms: "/terms",
   privacy: "/privacy",
+  resources: {
+    github: "https://github.com/forkbombx/reviewnow",
+    docs: "/docs",
+  },
 } as const;
