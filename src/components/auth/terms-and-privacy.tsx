00e08d3be6b838fa3a6ed56b0569d9e3d5f7c190
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:39 +0200

    feat(auth): Notify user when the magic link has been sent

--- src/components/auth/terms-and-privacy.tsx
+++ src/components/auth/terms-and-privacy.tsx
@@ -1,8 +1,11 @@
+import { routes } from "@/lib/routes";
+
 export function TermsAndPrivacy() {
   return (
     <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-      By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-      and <a href="#">Privacy Policy</a>.
+      By clicking continue, you agree to our{" "}
+      <a href={routes.terms}>Terms of Service</a> and{" "}
+      <a href={routes.privacy}>Privacy Policy</a>.
     </div>
   );
 }
