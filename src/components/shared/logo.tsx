3d74403b49ff50b4c4cf72f6932b326d0829d7c3
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:46 +0200

    feat(ui/auth): Updated the auth ui and logo

--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -3,12 +3,7 @@ import Image from "next/legacy/image";
 export function Logo() {
   return (
     <div className={"flex justify-start relative h-16 w-full"}>
-      <Image
-        layout="fill"
-        objectFit={"contain"}
-        src={"/logo.png"}
-        alt={"Reviewnow logo"}
-      />
+      <Image width={70} height={100} src={"/logo.png"} alt={"Reviewnow logo"} />
     </div>
   );
 }
