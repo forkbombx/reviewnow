d1af5c760e6d11ae15b90f52c6e726b76ef0d26f
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:35 +0200

    feat(oauth): Get Github oauth working

---
+++ src/components/auth/github-auth.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { FaGithub } from "react-icons/fa";
+import { useActionState } from "react";
+import { signInWithGithubAction } from "@/actions/auth";
+import { Loading } from "../ui/loading";
+
+export function GithubAuth() {
+  const [_, formAction, isPending] = useActionState(
+    signInWithGithubAction,
+    null,
+  );
+  return (
+    <form action={formAction}>
+      <Button
+        disabled={isPending}
+        variant="outline"
+        type="submit"
+        className="w-full"
+      >
+        {isPending ? <Loading /> : <FaGithub />}
+        Continue with Github
+      </Button>
+    </form>
+  );
+}
