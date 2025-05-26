4af9aac1bd43db7c536b9ddca01e704de8435439
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:39 +0200

    fear(dashboard): Created the dashboard layout

---
+++ src/components/dashboard/projects.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import {
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import { PanelsTopLeft, PlusCircle } from "lucide-react";
+
+export function Projects() {
+  return (
+    <SidebarGroup>
+      <SidebarGroupContent className="flex flex-col gap-2">
+        <SidebarMenu>
+          <SidebarMenuItem className="flex items-center gap-2">
+            <SidebarMenuButton
+              tooltip="Create Project"
+              className="bg-primary text-primary-foreground hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground min-w-8 duration-200 ease-linear"
+            >
+              <PlusCircle />
+              <span>Quick Create</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <SidebarMenuButton tooltip={"Project"}>
+              <PanelsTopLeft />
+              <span>big-mommy</span>
+            </SidebarMenuButton>
+          </SidebarMenuItem>
+        </SidebarMenu>
+      </SidebarGroupContent>
+    </SidebarGroup>
+  );
+}
