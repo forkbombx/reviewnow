fd98e02757a0970a037db0dc1a2205afa1ff46d4
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:35 +0200

    feat(oauthLogo): Added OAuth Logos according to regulations

--- src/components/auth/auth-form.tsx
+++ src/components/auth/auth-form.tsx
@@ -1,21 +1,23 @@
 "use client";
 
-import { GalleryVerticalEnd } from "lucide-react";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 import {
   Form,
   FormControl,
+  FormDescription,
   FormField,
   FormItem,
+  FormLabel,
   FormMessage,
 } from "@/components/ui/form";
-
+import { FaGithub } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
 import { authSchema } from "@/lib/form-schema";
 import { useForm } from "react-hook-form";
 import { z } from "zod";
-import { signIn } from "../../../auth";
 
 export function AuthForm() {
   const form = useForm<z.infer<typeof authSchema>>({
@@ -25,49 +27,38 @@ export function AuthForm() {
     },
   });
 
-  const onSubmit = async (values: z.infer<typeof authSchema>) => {
-    "use server";
-
-    const formData = new FormData();
-    formData.append("email", values.email);
-
-    await signIn("resend", formData);
-  };
-
   return (
     <div className={"flex flex-col gap-6"}>
       <div className="flex flex-col gap-6">
         <div className="flex flex-col items-center gap-2">
           <a href="#" className="flex flex-col items-center gap-2 font-medium">
-            <div className="flex size-8 items-center justify-center rounded-md">
-              <GalleryVerticalEnd className="size-6" />
-            </div>
+            <div className="flex size-8 items-center justify-center rounded-md"></div>
             <span className="sr-only">Reviewnow.</span>
           </a>
         </div>
 
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="grid gap-2">
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormControl>
-                    <Input placeholder="Email" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-
-            <div className="flex flex-col gap-6">
+        <div className="flex flex-col gap-6">
+          <Form {...form}>
+            <form>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="grid gap-2">
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Email" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
               <Button type="submit" className="w-full">
                 Login
               </Button>
-            </div>
-          </form>
-        </Form>
+            </form>
+          </Form>
+        </div>
       </div>
 
       <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
@@ -77,9 +68,11 @@ export function AuthForm() {
       </div>
       <div className="grid gap-4 sm:grid-cols-2">
         <Button variant="outline" type="button" className="w-full">
-          Continue with Apple
+          <FaGithub />
+          Continue with Github
         </Button>
         <Button variant="outline" type="button" className="w-full">
+          <FcGoogle />
           Continue with Google
         </Button>
       </div>
