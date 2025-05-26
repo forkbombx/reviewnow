1e4c0880edbea1d56c183760a1a78de3e994936e
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:47 +0200

    feat(dashboard): Created a create project section

--- src/components/dashboard/dashboard-header.tsx
+++ src/components/dashboard/dashboard-header.tsx
@@ -1,21 +1,32 @@
 import { Book } from "lucide-react";
 import { ToggleTheme } from "../shared/toggle-theme";
 import { Button } from "../ui/button";
-import { SearchBar } from "../ui/search-bar";
 import { UserDropdown } from "../shared/user-dropdown";
 import { Separator } from "../ui/separator";
+import { FaGithub } from "react-icons/fa";
+import Link from "next/link";
+import { routes } from "@/lib/routes";
 
 export function DashboardHeader() {
   return (
     <div className="border-b-2 border-dashed px-4 py-2 flex items-center justify-between">
       <UserDropdown />
       <div className="flex items-center gap-4">
-        <SearchBar />
-        <Button size="sm" variant="outline" className="hidden md:flex">
-          <Book />
-          <Separator orientation="vertical" />
-          Docs
-        </Button>
+        <Link href={routes.resources.docs}>
+          <Button size="sm" variant="outline">
+            <Book />
+            <Separator orientation="vertical" />
+            Docs
+          </Button>
+        </Link>
+        <Link href={routes.resources.github}>
+          <Button size="sm" variant="outline">
+            <FaGithub />
+            <Separator orientation="vertical" />
+            GitHub
+          </Button>
+        </Link>
+
         <ToggleTheme />
       </div>
     </div>
