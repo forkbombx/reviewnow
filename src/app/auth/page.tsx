0451c4e8ecbac2b6fcb870cde6cc0e9f5a662efd
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:46 +0200

    feat(error-alert): Created an ui error alert for auth flows

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,12 +3,14 @@ import { MagicLinkForm } from "@/components/auth/magic-link-form";
 import { TermsAndPrivacy } from "@/components/auth/terms-and-privacy";
 import { PageCentered } from "@/components/ui/page";
 import { Logo } from "@/components/ui/logo";
+import { ErrorAlert } from "@/components/ui/error-alert";
 
 export default function AuthPage() {
   return (
     <PageCentered>
       <div className={"flex flex-col gap-6"}>
         <Logo />
+        <ErrorAlert />
         <MagicLinkForm />
 
         <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
