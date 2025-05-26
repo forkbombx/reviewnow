d773624b4a465b8737cf8919e951ca8921788c73
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:46 +0200

    feat(oauth): Get Google oauth working

--- src/components/auth/google-auth.tsx
+++ src/components/auth/google-auth.tsx
@@ -1,13 +1,22 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { FcGoogle } from "react-icons/fc";
 import { useActionState } from "react";
+import { Loading } from "../ui/loading";
+import { signInWithGoogleAction } from "@/actions/auth";
 
 export function GoogleAuth() {
-  //  const [state, formAction, isPending] = useActionState()
+  const [_, formAction, isPending] = useActionState(
+    signInWithGoogleAction,
+    null,
+  );
   return (
-    <Button variant="outline" type="button" className="w-full">
-      <FcGoogle />
-      Continue with Google
-    </Button>
+    <form action={formAction}>
+      <Button variant="outline" type="submit" className="w-full">
+        {isPending ? <Loading /> : <FcGoogle />}
+        Continue with Google
+      </Button>
+    </form>
   );
 }
