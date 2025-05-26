134343fab10fd7c2c3c5ea056d625722229bdb0e
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:44 +0200

    feat(oauth): Added github and google oauth providers

--- auth.ts
+++ auth.ts
@@ -3,12 +3,16 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db/index";
 import Resend from "next-auth/providers/resend";
 import { env } from "@/env";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
   providers: [
-    Resend({
+    GitHub,
+    Google,
+    /*Resend({
       from: env.AUTH_FROM_MAIL,
-    }),
+    }),*/
   ],
 });
--- src/auth.ts
+++ src/auth.ts
@@ -3,6 +3,8 @@ import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db/index";
 import Resend from "next-auth/providers/resend";
 import { env } from "./env";
+import GitHub from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
@@ -10,5 +12,7 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     Resend({
       from: env.AUTH_FROM_MAIL,
     }),
+    GitHub,
+    Google,
   ],
 });
