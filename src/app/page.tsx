35d003913325616eef9f3dcc3cf081b55d0a4cdc
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:42 +0200

    feat(dashboard): Updated the dashboard layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,17 @@
-import { Menu } from "@/components/dashboard/menu";
-import { ProjectsGrid } from "@/components/dashboard/projects-grid";
 import { DashboardHeader } from "@/components/dashboard/dashboard-header";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { ProjectsGrid } from "@/components/dashboard/projects-grid";
 
 export default function Page() {
   return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <Menu variant="inset" />
-      <SidebarInset>
+    <div className="h-svh p-6 bg-sidebar-border">
+      <div className="min-h-full bg-background rounded-2xl @container/main flex flex-col w-full">
         <DashboardHeader />
         <div className="flex flex-1 flex-col">
           <div className="@container/main py-6 flex flex-1 flex-col gap-2">
             <ProjectsGrid />
           </div>
         </div>
-      </SidebarInset>
-    </SidebarProvider>
+      </div>
+    </div>
   );
 }
