5de7b97be06be7f02555f9318f5f36df25ce4bc8
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:39 +0200

    feat(auth): added DrizzleAdapter to the auth flow

--- app/db/index.ts
+++ app/db/index.ts
@@ -2,8 +2,6 @@ import { drizzle } from "drizzle-orm/neon-http"
 import { neon, neonConfig } from "@neondatabase/serverless"
 import ws from "ws"
 
-console.log(process.env.DATABASE_URL)
-
 neonConfig.webSocketConstructor = ws
 
 const sql = neon(process.env.DATABASE_URL!)
