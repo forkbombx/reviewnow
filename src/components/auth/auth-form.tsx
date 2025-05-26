d2afec93b32d9e6a891ed85fad02459e99846efa
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:35 +0200

    refactor(ui/auth): Refactored auth form to multiple components

--- src/components/auth/auth-form.tsx
+++ src/components/auth/auth-form.tsx
@@ -1,64 +1,15 @@
 "use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form";
-import { FaGithub } from "react-icons/fa";
-import { FcGoogle } from "react-icons/fc";
-import { authSchema } from "@/lib/form-schema";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
+import { OAuth } from "./oauth";
+import { MagicLinkForm } from "./magic-link-form";
+import { AuthFormHeader } from "./auth-form-header";
+import { TermsAndPrivacy } from "./terms-and-privacy";
 
 export function AuthForm() {
-  const form = useForm<z.infer<typeof authSchema>>({
-    resolver: zodResolver(authSchema),
-    defaultValues: {
-      email: "",
-    },
-  });
-
   return (
     <div className={"flex flex-col gap-6"}>
       <div className="flex flex-col gap-6">
-        <div className="flex flex-col items-center gap-2">
-          <a href="#" className="flex flex-col items-center gap-2 font-medium">
-            <div className="flex size-8 items-center justify-center rounded-md"></div>
-            <span className="sr-only">Reviewnow.</span>
-          </a>
-        </div>
-
-        <div className="flex flex-col gap-6">
-          <Form {...form}>
-            <form>
-              <FormField
-                control={form.control}
-                name="email"
-                render={({ field }) => (
-                  <FormItem className="grid gap-2">
-                    <FormLabel>Email</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Email" {...field} />
-                    </FormControl>
-                    <FormMessage />
-                  </FormItem>
-                )}
-              />
-              <Button type="submit" className="w-full">
-                Login
-              </Button>
-            </form>
-          </Form>
-        </div>
+        <AuthFormHeader />
+        <MagicLinkForm />
       </div>
 
       <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
@@ -66,21 +17,9 @@ export function AuthForm() {
           Or
         </span>
       </div>
-      <div className="grid gap-4 sm:grid-cols-2">
-        <Button variant="outline" type="button" className="w-full">
-          <FaGithub />
-          Continue with Github
-        </Button>
-        <Button variant="outline" type="button" className="w-full">
-          <FcGoogle />
-          Continue with Google
-        </Button>
-      </div>
 
-      <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
-      </div>
+      <OAuth />
+      <TermsAndPrivacy />
     </div>
   );
 }
