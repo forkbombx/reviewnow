dd3f8d5a1b37f9eadda56bd8bf2b4e842fef565b
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:46 +0200

    feat(auth): Notify user when the magic link has been sent

--- src/components/auth/magic-link-form.tsx
+++ src/components/auth/magic-link-form.tsx
@@ -3,15 +3,24 @@
 import { signInWithMagicLinkAction } from "@/actions/auth";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { useActionState } from "react";
+import { useActionState, useEffect } from "react";
 import { Label } from "@/components/ui/label";
 import { Loading } from "@/components/ui/loading";
+import { toast } from "sonner";
+import { useSearchParams } from "next/navigation";
 
 export function MagicLinkForm() {
+  const params = useSearchParams();
   const [state, formAction, isPending] = useActionState(
     signInWithMagicLinkAction,
     null,
   );
+
+  useEffect(() => {
+    if (params.get("type") === "email" && params.get("provider") === "resend") {
+      toast.success("We sent you a login link!", { dismissible: false });
+    }
+  }, [params]);
 
   return (
     <form className="grid gap-2" action={formAction}>
