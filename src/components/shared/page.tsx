c68f29b16abdc7a15161b11871c6fb0fa63a27ff
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:46 +0200

    feat(auth/ui): Updated th eUI of the auth page

--- src/components/shared/page.tsx
+++ src/components/shared/page.tsx
@@ -3,7 +3,7 @@ export function Page({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <div className="bg-background min-h-svh">{children}</div>;
+  return <div className="relative bg-background min-h-svh">{children}</div>;
 }
 
 export function PageCentered({
@@ -12,7 +12,7 @@ export function PageCentered({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+    <div className="relative bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       {children}
     </div>
   );
