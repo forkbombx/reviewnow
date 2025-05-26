08e75e07f866dd762b2b9675e2cd42adc48e7742
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:39 +0200

    feat(ErrorMap): Created an error map for auth errors

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -8,19 +8,21 @@ import { ErrorAlert } from "@/components/ui/error-alert";
 export default function AuthPage() {
   return (
     <PageCentered>
-      <div className={"flex flex-col gap-6"}>
-        <Logo />
-        <ErrorAlert />
-        <MagicLinkForm />
+      <div className="w-full max-w-sm">
+        <div className={"flex flex-col gap-6"}>
+          <Logo />
+          <ErrorAlert />
+          <MagicLinkForm />
 
-        <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-          <span className="bg-background text-muted-foreground relative z-10 px-2">
-            Or
-          </span>
+          <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+            <span className="bg-background text-muted-foreground relative z-10 px-2">
+              Or
+            </span>
+          </div>
+
+          <OAuth />
+          <TermsAndPrivacy />
         </div>
-
-        <OAuth />
-        <TermsAndPrivacy />
       </div>
     </PageCentered>
   );
