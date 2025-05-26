b8c078cb1a83c16da5f7850eef2dfb585bee7f19
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:39 +0200

    feat(auth/ui): Updated th eUI of the auth page

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,15 +1,24 @@
 import { MagicLinkForm } from "@/components/auth/magic-link-form";
 import { TermsAndPrivacy } from "@/components/auth/terms-and-privacy";
-import { PageCentered } from "@/components/ui/page";
-import { Logo } from "@/components/ui/logo";
+import { PageCentered } from "@/components/shared/page";
+import { Logo } from "@/components/shared/logo";
 import { ErrorAlert } from "@/components/ui/error-alert";
 import { GithubAuth } from "@/components/auth/github-auth";
 import { GoogleAuth } from "@/components/auth/google-auth";
+import { FlickeringGrid } from "@/components/magicui/flickering-grid";
 
 export default function AuthPage() {
   return (
     <PageCentered>
-      <div className="w-full max-w-sm">
+      <FlickeringGrid
+        className="absolute z-0 inset-0 size-full"
+        squareSize={5}
+        gridGap={6}
+        color="#6B7280"
+        maxOpacity={0.2}
+        flickerChance={0.5}
+      />
+      <div className="w-full max-w-sm z-10 p-4 rounded bg-background border shadow-lg">
         <div className={"flex flex-col gap-6"}>
           <Logo />
           <ErrorAlert />
--- src/components/shared/logo.tsx
+++ src/components/shared/logo.tsx
@@ -2,7 +2,7 @@ import Image from "next/legacy/image";
 
 export function Logo() {
   return (
-    <div className={"relative h-16 w-full"}>
+    <div className={"flex justify-start relative h-16 w-full"}>
       <Image
         layout="fill"
         objectFit={"contain"}
