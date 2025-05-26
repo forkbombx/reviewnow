e7d6570b36a1f2aa06e7bf11763e84394c18aa75
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:41 +0200

    fix(formData): Could not capture email from formData

--- src/components/auth/magic-link-form.tsx
+++ src/components/auth/magic-link-form.tsx
@@ -5,6 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { useActionState } from "react";
 import { Label } from "@/components/ui/label";
+import { Loading } from "@/components/ui/loading";
 
 export function MagicLinkForm() {
   const [state, formAction, isPending] = useActionState(signInAction, null);
@@ -12,12 +13,12 @@ export function MagicLinkForm() {
   return (
     <form className="grid gap-2" action={formAction}>
       <div className="grid gap-2">
-        <Label>Email</Label>
-        <Input placeholder="Email" />
+        <Label htmlFor="email">Email</Label>
+        <Input name="email" placeholder="Email" />
         <p className="text-red-500"> {state?.message} </p>
       </div>
       <Button disabled={isPending} type="submit" className="w-full">
-        Login
+        {isPending && <Loading />} Login
       </Button>
     </form>
   );
---
+++ src/components/ui/loading.tsx
@@ -0,0 +1,5 @@
+import { AiOutlineLoading } from "react-icons/ai";
+
+export function Loading() {
+  return <AiOutlineLoading className="size-5 animate-spin" />;
+}
