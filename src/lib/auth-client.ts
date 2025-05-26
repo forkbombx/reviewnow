694e122f946f92b1885e0d02c668977ad6f3d9ea
a2shn <209373144+a2shn@users.noreply.github.com>
2025-05-26 07:04:44 +0200

    refactor(auth): Roolback OneTap support

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,11 +1,6 @@
 import { createAuthClient } from "better-auth/react";
-import { magicLinkClient, oneTapClient } from "better-auth/client/plugins";
+import { magicLinkClient } from "better-auth/client/plugins";
 import { env } from "@/env";
 export const authClient = createAuthClient({
-  plugins: [
-    magicLinkClient(),
-    oneTapClient({
-      clientId: env.AUTH_GOOGLE_ID,
-    }),
-  ],
+  plugins: [magicLinkClient()],
 });
